c738a07a3e33a292db8dd59c3f238f6c3f02458a
duggavo <>
2024-04-30 14:45:37 +0200

    fix build script

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ mkdir build
 cp LICENSE.txt ./build/
 cd build
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build .. -trimpath -ldflags="-s -w"
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -trimpath -ldflags="-s -w" ..
 
 tar -czf xatum-proxy-linux.tar.gz xatum-proxy LICENSE.txt
 
-GOOS=windows GOARCH=amd64 go build .. -trimpath -ldflags="-s -w"
+GOOS=windows GOARCH=amd64 go build -trimpath -ldflags="-s -w" ..
 
 zip xatum-proxy-windows.zip xatum-proxy.exe LICENSE.txt
